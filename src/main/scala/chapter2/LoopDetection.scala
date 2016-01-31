f786473d43eb37ad5bc9f69cd6f642217d7f0848
Paolo Rascuna <prascuna@gmail.com>
2016-01-31 10:30:19 +0000

    Exercise 2.8

--- src/main/scala/chapter2/LoopDetection.scala
+++ src/main/scala/chapter2/LoopDetection.scala
@@ -9,24 +9,14 @@ object LoopDetection {
 
   def verify[T](list: LinkedList[T]): Option[Node[T]] = {
 
-//    def search[T](node: Option[Node[T]], nodes: Set[T]): Option[Node[T]] = {
-    def search[T](node: Option[Node[T]], nodes: Set[Node[T]]): Option[Node[T]] = {
+    def search[T](node: Option[Node[T]], nodes: Set[Int]): Option[Node[T]] = {
       node match {
-//        case Some(n) if nodes.contains(n.value) => {
-        case Some(n) if nodes.contains(n) => {
-          println(n.value)
-          Some(n)
-        }
-        case Some(n) => {
-          println(n.value)
-//          search(n.next, nodes + n.value)
-          search(n.next, nodes + n)
-        }
+        case Some(n) if nodes.contains(System.identityHashCode(n)) => Some(n)
+        case Some(n) => search(n.next, nodes + System.identityHashCode(n))
         case None => None // This should never happen if it's a circular list
       }
     }
-//    search(list.head, Set[T]())
-    search(list.head, Set[Node[T]]())
+    search(list.head, Set[Int]())
   }
 
 }
